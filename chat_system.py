9def949159a661bd967b5486872e9236ac088eb2
Prime7777 <geeky2781@gmail.com>
2026-06-11 13:17:05 +0530

    Update chat_system.py

--- chat_system.py
+++ chat_system.py
@@ -1,61 +1,29 @@
-# # OOP Chat System Project
-# class User:
-#     def __init__(self,username):
-#         self.username=username
-    
-#     def join_chatroom(self):
-#         print(f"{self.username} joins the chatroom.")
-    
-#     def leave_chatroom(self):
-#         print(f"{self.username} left the chatroom.")
-    
-#     def send_message(self,send_msg):
-#         print(f"send message: {send_msg}")
-    
-# class Message:
-#     def __init__(self,sender,content,timestamp,new_msg):
-#         self.sender=sender
-#         self.content=content
-#         self.timestamp=timestamp
-#         self.new_msg=new_msg
-#     def show_display(self):
-#         print(f"New Message: {self.new_msg}")
-    
-# class Chatroom:
-#     def __init__(self,room_name,users,chat_history):
-#         self.room_name=room_name
-#         self.users=users
-#         self.chat_history=chat_history
-#     def add_users(self,add):
-#         self.users=self.users.append(add)
-#         print(f"{add} is added by the owner.")
-#     def remove_user(self,remove):
-#         self.users = self.users - remove
-#         print(f"{remove} is removed by the owner.")
-    
-
-
-   
 class Message:
-    message_counter = 1   # simple counter
+    message_counter = 1  # simple counter
+
     def __init__(self, sender, content):
         self.sender = sender
         self.content = content
         self.id = Message.message_counter
         Message.message_counter += 1
-        def __str__(self):
-            return f"({self.id}) {self.sender.username}: {self.content}"
+
+    def __str__(self):
+         return f"({self.id}) {self.sender.username}: {self.content}"
+
+
 class User:
     def __init__(self, username):
         self.username = username
         self.chatroom = None
-        def join_chatroom(self, chatroom):
-            if self.chatroom:
-                print(f"{self.username} is already in a chatroom.")
-            else:
-                chatroom.add_user(self)
-                self.chatroom = chatroom
-                print(f"{self.username} joined {chatroom.name}")
+
+    def join_chatroom(self, chatroom):
+         if self.chatroom:
+            print(f"{self.username} is already in a chatroom.")
+         else:
+            chatroom.add_user(self)
+            self.chatroom = chatroom
+            print(f"{self.username} joined {chatroom.name}")
+
     def leave_chatroom(self):
         if not self.chatroom:
             print(f"{self.username} is not in any chatroom.")
@@ -63,26 +31,50 @@ class User:
             self.chatroom.remove_user(self)
             print(f"{self.username} left {self.chatroom.name}")
             self.chatroom = None
-        def send_message(self, content):
-            if not self.chatroom:
-                print(f"{self.username} cannot send a message (not in a chatroom).")
-            else:
-                self.chatroom.broadcast(self, content)
+
+    def send_message(self, content):
+        if not self.chatroom:
+            print(f"{self.username} cannot send a message (not in a chatroom).")
+        else:
+            self.chatroom.broadcast(self, content)
+
+
 class ChatRoom:
     def __init__(self, name):
         self.name = name
         self.users = []
         self.messages = []
+
     def add_user(self, user):
         self.users.append(user)
+
     def remove_user(self, user):
         self.users.remove(user)
+
     def broadcast(self, sender, content):
         message = Message(sender, content)
         self.messages.append(message)
-        print(message)   
+        print(message)
+
     def show_chat_history(self):
         print(f"\nChat History of {self.name}:")
         for msg in self.messages:
             print(msg)
+        print()
 
+
+if __name__ == "__main__":
+    room = ChatRoom("Python Lounge")
+    u1 = User("Alice")
+    u2 = User("Bob")
+    u3 = User("Charlie")
+    u1.join_chatroom(room)
+    u2.join_chatroom(room)
+    u1.send_message("Hello everyone!")
+    u2.send_message("Hi Alice!")
+    u3.join_chatroom(room)
+    u3.send_message("Hey guys, what's up?")
+    room.show_chat_history()
+    u1.leave_chatroom()
+    u2.leave_chatroom()
+    u3.leave_chatroom()
